dceca4a824957708387c007d27fc1f84536b183f
himanshu0072 <himanshu2003prajapati@gmail.com>
2024-09-04 17:40:59 +0530

    learning javascript

---
+++ BASICJS/index.js
@@ -0,0 +1,14 @@
+// javascript is a dynamically types programming language
+// Variable
+// var, let, const - Interview
+// Hoisting - Interview - Explaination of Hoisting
+// var, let and const me difference
+
+var age = 1;
+console.log(age);
+console.log(typeof age);
+{
+  let age = "hello";
+  console.log(age);
+  console.log(typeof age);
+}
