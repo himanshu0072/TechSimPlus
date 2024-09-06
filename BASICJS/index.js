c889f98975fb05cdee79414d0ed2567782469211
himanshu0072 <himanshu2003prajapati@gmail.com>
2024-09-06 18:42:09 +0530

    day 4

--- BASICJS/index.js
+++ BASICJS/index.js
@@ -1,14 +1,59 @@
+// 2nd day
 // javascript is a dynamically types programming language
 // Variable
 // var, let, const - Interview
 // Hoisting - Interview - Explaination of Hoisting
 // var, let and const me difference
 
-var age = 1;
-console.log(age);
-console.log(typeof age);
-{
-  let age = "hello";
-  console.log(age);
-  console.log(typeof age);
-}
+// var age = 1;
+// console.log(age);
+// console.log(typeof age);
+// {
+//   let age = "hello";
+//   console.log(age);
+//   console.log(typeof age);
+// }
+
+// hoisting iws a mechanism in js. whenever we declaire any varriable in js the declairation of variable is declaired at the top of program.. not the value before execution
+
+// 3rd day
+
+// console.log(email);
+// var email = null;
+
+//operators
+
+// let m1 = 20;
+// let m2 = "10";
+
+// console.log(m1 - -m2);
+
+// day 4
+// js is a dynamicaaly typed lang
+
+// console.log(2 % 10);
+
+// WAP to check prime number
+
+// n = 10;
+// flag = 1;
+// for (let index = 2; index < n - 1; index++) {
+//   if (n / index === 0) {
+//     flag = 0;
+//   } else {
+//     flag = 1;
+//   }
+// }
+// if (flag === 1) {
+//   console.log("Prime");
+// } else {
+//   console.log("Not Prime");
+// }
+
+// let res = console.log("Priyanshi💓") !== null && 12 && console.log("Hii");
+// console.log(res);
+
+var email = "bbc@gmail.com";
+var lengths = email.length;
+lengths >= 10 && console.log(email);
+
